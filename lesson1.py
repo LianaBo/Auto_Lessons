5476e94d2414c246d4fb59c8b1653cf627c68d9b
LianaBo <liana.bobrikova@bk.ru>
2021-06-20 14:38:22 +0300

    Скорость 0.9

--- lesson1.py
+++ lesson1.py
@@ -6,18 +6,21 @@ import os
 file = "GoodFile.txt"
 path = "C:\\Users\\User\\Desktop\\Python\\" + file
 
+if os.path.exists(file):
+    os.remove(file) 
+
 pg.hotkey("winleft")
 time.sleep(2)
 
-pg.typewrite("notepad\n", 0.1)
+pg.typewrite("notepad\n", 0.9)
 time.sleep(1)
 
 expect = "Good job"
-pg.typewrite(expect, 0.1)
+pg.typewrite(expect, 0.9)
 pg.hotkey("ctrl", "s")
 time.sleep(1)
 
-pg.typewrite(path + "\n", 0.1)
+pg.typewrite(path + "\n", 0.9)
 
 actual = ""
 with open(path) as inf:
